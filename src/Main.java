f031f6f683ba3d6b8e9fb766122ad98f31bb7acc
ardaywcgl <yasirardaayvacioglu@gmail.com>
2024-02-25 00:50:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // to see on console=System.out.println(a);
+        // byte a = 34; => it is a variable between 128 and -127
+        // short a = 34; => it is a variable between 32,767 and -32,768
+        // int a = 34; => it is a variable between bla bla bla and -bla bla bla(most using variable)
+        // long a = 34; => it is a variable when int is not enough use long
+        // char a = s; => it is a variable for just one character
+        // boolean a = true; => it is a variable for true and false
+        // float a = 34,324; => it is a variable for  decimal numbers
+        // double a = 34; => it is a variable for big mathematical operations
+        // string a = asfpüksdüfpkasüpdgks; => it is a variable for texts
+
+    }
+}
